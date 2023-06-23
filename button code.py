033cb4d4c4de1fa3b25d94a8665a72e3d1ad95b2
syahmisanab <115781685+syahmisanab@users.noreply.github.com>
2023-06-23 16:01:30 +0800

    Update button code.py

--- button code.py
+++ button code.py
@@ -1,21 +1,16 @@
-int buttonState = 0;
+# connect button with gpio 25 and gnd 
+# onboard arduino nano rp2040 is on pin 6
 
-void setup()
-{
-  pinMode(2, INPUT);
-  pinMode(13, OUTPUT);
-}
 
-void loop()
-{
-  // Initialize button state with LOW or 0
-  buttonState = 0;
-  // Read State of Pin number 2
-  buttonState = digitalRead(2);
-  if (buttonState == 1) {
-    digitalWrite(13, HIGH);
-    delay(1000); // Wait for 1000 millisecond(s)
-  } else {
-    digitalWrite(13, LOW);
-  }
-}
+import machine
+import time
+
+button = machine.Pin(25, machine.Pin.IN, machine.Pin.PULL_UP)
+led = machine.Pin(6, machine.Pin.OUT)
+
+while True:
+    if button.value() == 1:  # Button is pressed
+        led.value(0)  # Turn on the LED
+    else:
+        led.value(1)  # Turn off the LED
+    time.sleep(0.1)  # Add a small delay to avoid rapid toggling
